4734b36d006d26d9ff4f8bd227819b9271211cba
fifovalle <fifanaufal10@gmail.com>
2024-12-12 14:26:06 +0700

    Selesai

--- app/api/hapus-admin/route.js
+++ app/api/hapus-admin/route.js
@@ -3,25 +3,13 @@ import admin from "firebase-admin";
 import { doc, deleteDoc } from "firebase/firestore";
 import { database } from "@/lib/firebaseConfig";
 
-const serviceAccount = process.env.FIREBASE_CREDENTIALS
-  ? JSON.parse(process.env.FIREBASE_CREDENTIALS)
-  : null;
-
-if (!serviceAccount) {
-  throw new Error(
-    "Environment variable FIREBASE_CREDENTIALS is not defined or invalid."
-  );
-}
+const firebaseCredentials = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 
 if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      projectId: "ptsp-bmkg-4eee9",
-    });
-  } catch (error) {
-    console.error("Firebase admin initialization error", error);
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseCredentials),
+    projectId: "ptsp-bmkg-4eee9",
+  });
 }
 
 export async function POST(req) {
